1b2f79736adb5f5d2581c36b0b7550a424d42af2
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-08-11 11:28:54 +0100

    Created player searching and scene changing on match found

---
+++ Assets/Scripts/PlayerNameInput.cs
@@ -0,0 +1,44 @@
+﻿using Photon.Pun;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TurnBased
+{
+    public class PlayerNameInput : MonoBehaviour
+    {
+        [Required] [SerializeField] private TMP_InputField nameInputField = null;
+        [Required] [SerializeField] private Button continueButton = null;
+
+        private const string PlayerNamePrefKey = "PlayerName";
+
+        private void Start() => SetUpInputField();
+
+        private void SetUpInputField()
+        {
+            if (!PlayerPrefs.HasKey(PlayerNamePrefKey)) { return; }
+
+            string defaultName = string.Empty;
+
+            defaultName = PlayerPrefs.GetString(PlayerNamePrefKey);
+
+            nameInputField.text = defaultName;
+            SetPlayerName(defaultName);
+        }
+
+        public void SetPlayerName(string name)
+        {
+            continueButton.interactable = !string.IsNullOrEmpty(name);
+        }
+
+        public void SavePlayerName()
+        {
+            string playerName = nameInputField.text;
+
+            PhotonNetwork.NickName = playerName;
+
+            PlayerPrefs.SetString(PlayerNamePrefKey, playerName);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerNamesDisplay.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace TurnBased
+{
+    public class PlayerNamesDisplay : MonoBehaviour
+    {
+        [Required] [SerializeField] private TextMeshProUGUI myNameText = null;
+        [Required] [SerializeField] private TextMeshProUGUI opponentNameText = null;
+
+        private void Start()
+        {
+            myNameText.text = PhotonNetwork.NickName;
+            opponentNameText.text = PhotonNetwork.PlayerListOthers[0].NickName;
+        }
+    }
+}
